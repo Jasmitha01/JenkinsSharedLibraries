7d4da67ae6a2ab3cc28a92f6a98f58e860f52efe
Jasmitha <jasmitha@Echidnas-MacBook-Pro.local>
2018-02-01 20:07:43 +0530

    first extendended shared library

---
+++ src/org/mine/first/Hello.groovy
@@ -0,0 +1,21 @@
+ipackage org.mine.first
+import org.mine.*
+
+class Hello implements Serializable {
+  def config
+  def script
+
+  Hello(script,config) {
+    this.config = config
+    this.script = script
+  }
+
+  
+  void wish()
+  {
+	this.script.stage('wish'){
+		print "Hello"
+	}	
+  }
+
+}
