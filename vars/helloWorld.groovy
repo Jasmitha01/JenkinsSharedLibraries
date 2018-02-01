0a40cf1015edcbf1c6a820f48d6d47f0e07ac523
Jasmitha <jasmitha@Echidnas-MacBook-Pro.local>
2018-02-01 14:11:07 +0530

    first file

---
+++ vars/helloWorld.groovy
@@ -0,0 +1,14 @@
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    node {
+	stage('stage-1'){
+		echo "Hello %{config.name}"
+	}
+    }
+}
+
