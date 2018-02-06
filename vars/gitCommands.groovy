c1880418096cfe9e119082772e76fe5e662b170d
Jasmitha <m.jasmitha@gmail.com>
2018-02-06 18:25:48 +0530

    Update gitCommands.groovy

--- vars/gitCommands.groovy
+++ vars/gitCommands.groovy
@@ -6,12 +6,22 @@ def call(body) {
     body()
 
     node{
-           stage('checkout') {
+           stage('Checkout') {
               git(url: config.url, branch: config.branch, credentialsId: config.cid)
            }
-            stage('checkout') {
+        
+           stage('Create temp file') {
               sh('cd $WORKSPACE/')
               sh('touch '+config.file)
+           }
+         
+          stage('Commit'){
+              sh('git add .")
+              sh('git commit -m "adding the file "' + config.file)
+           }
+                 
+          stage('Push'){
+              sh("git push origin "+config.branch)
            }
     }
 
