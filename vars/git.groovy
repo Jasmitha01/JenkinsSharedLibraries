1851d342ded7f77bd7d6ea053f969e985670510f
Jasmitha <m.jasmitha@gmail.com>
2018-02-06 17:57:31 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -7,7 +7,7 @@ def call(body) {
 
     node{
            stage('checkout') {
-               git url: ${config.url}
+               git url: 'https://github.com/JasmithaMeka/JenkinsSharedLibraries.git'
            }
     }
 
